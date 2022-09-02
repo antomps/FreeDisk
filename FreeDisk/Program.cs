86661eef4beac4499cc2a76cbc7ea5f71e002ad2
lamacias25 <la.macias@mavi.mx>
2022-09-02 11:26:02 -0500

    initial commit

---
+++ FreeDisk/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace FreeDisk
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Punto de entrada principal para la aplicación.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            TrayIcon trayIcon = new TrayIcon();
+
+            Application.Run();
+        }
+    }
+}
---
+++ FreeDisk/TrayIcon.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace FreeDisk
+{
+    class TrayIcon
+    {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool DestroyIcon(IntPtr handle);
+
+        private const string iconFont = "Segoe UI";
+        private const int iconFontSize = 14;
+
+        private Color colorText;
+        private NotifyIcon notifyIcon;
+
+        public TrayIcon()
+        {
+            ContextMenu contextMenu = new ContextMenu();
+            MenuItem menuItem = new MenuItem();
+
+            notifyIcon = new NotifyIcon();
+
+            // initialize contextMenu
+            contextMenu.MenuItems.AddRange(new MenuItem[] { menuItem });
+
+            // initialize menuItem
+            menuItem.Index = 0;
+            menuItem.Text = "E&xit";
+            menuItem.Click += new System.EventHandler(menuItem_Click);
+
+            notifyIcon.ContextMenu = contextMenu;
+
+            notifyIcon.Visible = true;
+
+            Timer timer = new Timer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = 10000; // in miliseconds
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            DriveInfo drive = new DriveInfo("C");
+            double spaceFree = drive.AvailableFreeSpace / (double)1073741824;
+            int space = (int)spaceFree;
+
+            if (space > 29)
+            {
+                colorText = Color.Green;
+            }
+            else if (space > 19 && space < 30)
+            {
+                colorText = Color.Yellow;
+            }
+            else
+            {
+                colorText = Color.Red;
+            }
+
+            using (Bitmap bitmap = new Bitmap(DrawText(space.ToString(), new Font(iconFont, iconFontSize, FontStyle.Bold), colorText, Color.Transparent)))
+            {
+                System.IntPtr intPtr = bitmap.GetHicon();
+                try
+                {
+                    using (Icon icon = Icon.FromHandle(intPtr))
+                    {
+                        notifyIcon.Icon = icon;
+                        notifyIcon.Text = space + "G Free";
+                    }
+                }
+                finally
+                {
+                    DestroyIcon(intPtr);
+                }
+            }
+        }
+
+        private void menuItem_Click(object sender, EventArgs e)
+        {
+            notifyIcon.Visible = false;
+            notifyIcon.Dispose();
+            Application.Exit();
+        }
+
+        private Image DrawText(String text, Font font, Color textColor, Color backColor)
+        {
+            var textSize = GetImageSize(text, font);
+            Image image = new Bitmap((int)textSize.Width, (int)textSize.Height);
+            using (Graphics graphics = Graphics.FromImage(image))
+            {
+                // paint the background
+                graphics.Clear(backColor);
+
+                // create a brush for the text
+                using (Brush textBrush = new SolidBrush(textColor))
+                {
+                    graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+                    graphics.DrawString(text, font, textBrush, 0, 0);
+                    graphics.Save();
+                }
+            }
+
+            return image;
+        }
+
+        private static SizeF GetImageSize(string text, Font font)
+        {
+            using (Image image = new Bitmap(1, 1))
+            using (Graphics graphics = Graphics.FromImage(image))
+                return graphics.MeasureString(text, font);
+        }
+    }
+}
